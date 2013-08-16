b672ae817ed8de58eb032b699ab7b7ebec19bbac
Musselman <kmussel@gmail.com>
2013-08-16 10:41:40 -0400

    updated edge instance and orientdb-jruby gem

--- lib/oriented/relationships/edges_instance.rb
+++ lib/oriented/relationships/edges_instance.rb
@@ -19,16 +19,7 @@ module Oriented
       end
 
       def to_other(other_vertex)
-        dir = @rel_type.direction == Oriented::Relationships::Direction::IN ? "in" : "out"
-        other_dir = dir == "out" ? "in" : "out"
-        f =  vertex.record.field("#{dir}_#{@rel_type.label}")
-        rec = case f
-        when Java::ComOrientechnologiesOrientCoreTypeTree::OMVRBTreeRIDSet
-          f.select {|e| e.field(other_dir).get_identity().to_s == (other_vertex.id) }.first
-        else
-          f.field(other_dir) if f.field(other_dir).get_identity().to_s == (other_vertex.id)
-        end
-        OrientDB::BLUEPRINTS::impls::orient::OrientEdge.new(vertex.graph, rec) if rec
+        vertex.get_edges(other_vertex.__java_obj, @rel_type.direction).to_a
       end
 
 
