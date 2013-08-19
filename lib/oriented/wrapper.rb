c473dfe88b0eb7447e0d468a1cabd27a22471f16
Glenn Goodrich <glenn.goodrich@gmail.com>
2013-08-19 16:17:36 -0400

    Check Object for constants too in wrapper

--- lib/oriented/wrapper.rb
+++ lib/oriented/wrapper.rb
@@ -15,7 +15,7 @@ module Oriented
       clname = Oriented::Registry.ruby_class_for(classname)
       return java_obj if clname[0].nil?
       clname = clname[0].upcase + clname[1..-1]
-      return java_obj if java_obj.get_element_type == "Edge" && !Kernel.const_defined?(clname) 
+      return java_obj if java_obj.get_element_type == "Edge" && !(Kernel.const_defined?(clname) && Object.const_defined?(clname))
       new_instance = to_class(clname).orig_new
 
       new_instance.__java_obj = java_obj
