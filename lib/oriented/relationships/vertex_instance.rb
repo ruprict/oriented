9e567eb1df4cd74c5124e8786ae614efb3260d7c
Glenn Goodrich <glenn.goodrich@gmail.com>
2013-08-19 13:25:52 -0400

    Wrap the return value of create_relationship_to

--- lib/oriented/relationships/vertex_instance.rb
+++ lib/oriented/relationships/vertex_instance.rb
@@ -34,7 +34,7 @@ module Oriented
       end
 
       def create_relationship_to(other, attrs={})
-        vertex.add_edge(@rel_type.label.to_s, other.__java_obj, nil, nil, attrs)
+        vertex.add_edge(@rel_type.label.to_s, other.__java_obj, nil, nil, attrs).wrapper
       end
 
       def destroy_relationship
